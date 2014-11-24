fa2a755927cc6689345653c0fa90423ad8643987
Yanir Taflev <yanir.taflev@applitools.com>
2014-11-24 19:22:53 +0200

    DotNet selenium examples

---
+++ DotNet/Console.Example/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Drawing;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using Applitools;
+
+namespace MyTests
+{
+    public class TestApplitoolsWebsite
+    {
+        public static void Main(string[] args)
+        {
+            IWebDriver driver = new FirefoxDriver();
+
+            // This is your api key, make sure you use it in all your tests.
+            var eyes = new Eyes();
+            eyes.ApiKey = APPLITOOLS_APIKEY;
+
+            try
+            {
+                driver.Navigate().GoToUrl("http://www.applitools.com");
+
+                // Start visual testing with browser viewport set to 1024x768.
+                // Make sure to use the returned driver from this point on.
+                driver = eyes.Open(driver, "Applitools website", "Example test", new Size(900, 600));
+
+                // Visual validation point #1
+                eyes.CheckWindow("Main Page");
+
+                driver.FindElement(By.ClassName("read_more")).Click();
+
+                // Visual validation point #2
+                eyes.CheckWindow("Features Page");
+
+                // End visual testing. Validate visual correctness.
+                eyes.Close();
+            }
+            finally
+            {
+                eyes.AbortIfNotClosed();
+                driver.Quit();
+            }
+        }
+    }
+}
---
+++ DotNet/Unitest.Example/ApplitoolsWebsiteTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Applitools;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Drawing;
+
+namespace Examples
+{
+    [TestClass]
+    public class ApplitoolsWebsiteTest
+    {
+        private IWebDriver driver;
+        private Eyes eyes;
+
+        [TestInitialize]
+        public void Init()
+        {
+            eyes = new Eyes();
+            eyes.ApiKey = APPLITOOLS_APIKEY;
+            driver = new ChromeDriver();
+        }
+
+        [TestMethod]
+        public void TestApplitoolsWebsite()
+        {
+            driver.Navigate().GoToUrl("http://www.applitools.com");
+
+            driver = eyes.Open(driver, "Applitools website", "Example test", new Size(900, 600));
+            
+            // Visual validation point #1
+            eyes.CheckWindow("Main Page");
+            
+            driver.FindElement(By.ClassName("read_more")).Click();
+            
+            // Visual validation point #2
+            eyes.CheckWindow("Features Page");
+            
+            eyes.Close();
+        }
+
+        [TestCleanup]
+        public void TearDown()
+        {
+            eyes.AbortIfNotClosed();
+            driver.Quit();
+        }
+
+    }
+}
